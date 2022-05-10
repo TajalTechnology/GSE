60f5c803d5d49a9f19ace612966adb2638139de9
TajalTechnology <mrpresident1189@gmail.com>
2022-05-11 00:23:43 +0600

    added easy leetcode 35No problem

--- dalgo/binary-search/1.binary-search.js
+++ dalgo/binary-search/1.binary-search.js
@@ -1,17 +1,17 @@
 function binarySearch(arrayNum, target) {
   let lowIndex = 0;
   let highIndex = arrayNum.length - 1;
-  let midIndex;
+
   /**
    *
    */
   while (highIndex >= lowIndex) {
-    midIndex = Math.ceil((lowIndex + highIndex) / 2);
-    if (arrayNum[midIndex] == target) return midIndex;
-    if (arrayNum[midIndex] > target) highIndex = midIndex;
-    else lowIndex = midIndex + 1;
+    let midIndex = Math.ceil((highIndex + lowIndex) / 2);
+    if (arrayNum[midIndex] === target) return midIndex;
+    else if (arrayNum[midIndex] < target) lowIndex = midIndex + 1;
+    else highIndex = midIndex - 1;
   }
 }
 
-const result = binarySearch([2, 3, 4, 10, 40], 2);
+const result = binarySearch([2, 3, 4, 11, 41], 12);
 console.log(result);
--- dalgo/binary-search/2.easy-leetcode-35.js
+++ dalgo/binary-search/2.easy-leetcode-35.js
@@ -8,37 +8,14 @@ function binarySearch_leetcode_35(arrayNum, target) {
   while (highIndex >= lowIndex) {
     midIndex = Math.ceil((lowIndex + highIndex) / 2);
 
-    if (arrayNum[midIndex] == target) return midIndex;
-    if (arrayNum[highIndex] < target) return highIndex + 1;
-
-    if (arrayNum[midIndex] > target) highIndex = midIndex;
+    console.log("Low:", lowIndex, "High:", highIndex);
+    if (target > arrayNum[highIndex]) return highIndex + 1;
+    else if (target < arrayNum[lowIndex]) return lowIndex;
+    else if (target === arrayNum[midIndex]) return midIndex;
     else if (arrayNum[midIndex] < target) lowIndex = midIndex + 1;
-    else if (arrayNum[highIndex] > target && arrayNum[lowIndex] < target)
-      return lowIndex + 1;
-    console.log(Math.ceil(midIndex));
+    else highIndex = midIndex - 1;
   }
 }
 
-const result = binarySearch_leetcode_35([2, 3, 4, 10, 40], 11);
+const result = binarySearch_leetcode_35([1, 3, 5, 6], 7);
 console.log(result);
-
-// function findIndex(num, target) {
-//   let lowIndex = 0;
-//   let hightIndex = num.length - 1;
-//   let targetIndex;
-//   let midIndex;
-
-//   while (hightIndex >= lowIndex) {
-//     midIndex = Math.ceil((lowIndex + hightIndex) / 2);
-
-//     if (target > num[hightIndex]) return (targetIndex = hightIndex + 1);
-
-//     if (target < num[midIndex]) hightIndex = num[midIndex];
-//     if (target > num[midIndex] && target < num[hightIndex])
-//       lowIndex = num[midIndex + 1];
-//     if (target == num[midIndex]) return (targetIndex = midIndex);
-//   }
-// }
-
-// const result = findIndex([1, 3, 5, 6], 1);
-// console.log(result);
