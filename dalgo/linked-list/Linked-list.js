7eeb8d8508c5d8c8372b1af0dbdea0ba4afd136c
TajalTechnology <mrpresident1189@gmail.com>
2023-08-20 22:37:06 +0600

    ADD: pop in linked list

--- dalgo/linked-list/Linked-list.js
+++ dalgo/linked-list/Linked-list.js
@@ -13,13 +13,27 @@ class List{
 
     push(value){
         const node = new Node(value);
-        if(this.head === null) this.head = node
+        if(this.head === null) this.head = node;
         else{
             let current = this.head;
             while(current.next) current = this.head.next;
             current.next = node
         }
-        console.log(this.head) // show list
+        this.size++;
+        console.log(this.size,':::', this.head);
+    }
+
+    pop(){
+        if(this.head === null) return;
+        let current = this.head;
+        if(current?.next){
+            while(current.next){
+                if(current.next.next === null) current.next = null
+                else current = current.next;
+            }
+        }else this.head = null;
+        this.size --;
+        console.log('36',this.size,':::', this.head);
     }
 }
 
@@ -27,3 +41,7 @@ const linkedList = new List();
 linkedList.push(10)
 linkedList.push(20)
 linkedList.push(30)
+linkedList.pop()
+linkedList.pop()
+linkedList.pop()
+linkedList.pop()
