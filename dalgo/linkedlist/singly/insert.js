8445956a5d493b760d5c31f8a079ff3f2af6c602
TajalTechnology <mrpresident1189@gmail.com>
2024-03-19 03:01:54 +0600

    ADD: delete at index

--- dalgo/linkedlist/singly/insert.js
+++ dalgo/linkedlist/singly/insert.js
@@ -25,23 +25,52 @@ class LinkListMethods{
         const newList = new Node(value);
         newList.nextAddress = this.head
         this.head = newList;
-        console.log(`========<>insertAtFirst`,this.head);
-
     }
+
     insertAtPosition(value, position){
         const node = new Node(value);
         let currentNode = this.head;
-        let length = 1;
+        let length = 1; 
 
-        while(currentNode.nextAddress){
-            length++;
-            if(length === position) {
-                node.nextAddress = currentNode.nextAddress;
-                currentNode.nextAddress = node; 
-                break;
+        if(position === 1) {
+            node.nextAddress = currentNode;
+            this.head = node
+        } else {
+            while(currentNode?.nextAddress){
+                length++;
+                if(length === position) {
+                    node.nextAddress = currentNode?.nextAddress;
+                    currentNode.nextAddress = node; 
+                    break;
+                }
+                currentNode = currentNode?.nextAddress;
+            }
+            if((length + 1) === position){
+                currentNode.nextAddress = node;
+                length++;
             }
         }
+        if(position > length) console.log('index not found')
         console.log(`========<>insertAtPosition ${position}`,this.head);
+    }
+
+    deleteAtIndex(index){
+        let len = 1;
+        let currentNode = this.head;
+        if(len === index)this.head = currentNode.nextAddress;
+        else{
+            let currentNode = this.head;
+            while(currentNode?.nextAddress){
+                len++;
+                if(len === index){
+                    currentNode.nextAddress = currentNode?.nextAddress?.nextAddress;
+                    break;
+                }
+                currentNode = currentNode.nextAddress;
+            }
+            if(len < index) console.log('Not found')
+        }
+        console.log(`========<>deleteAtIndex ${index}`,this.head);
     }
 }
 
@@ -51,3 +80,4 @@ list.insertAtEnd(3)
 list.insertAtEnd(1)
 list.insertAtPosition(9, 2)
 list.insertAtFirst(10)
+list.deleteAtIndex(4);
